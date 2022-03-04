b829c039efc60648a0fa60032415471821eaff26
Ethan Bradley <elmb201@gmail.com>
2022-03-04 13:14:25 -0800

    Updated ls.js, bash.js, and added cat.js

--- bash.js
+++ bash.js
@@ -1,18 +1,27 @@
+let lsRequired = require('./ls');
 let required = require('./pwd');
-let lsRequired = require ('./ls')
+let catRequired = require('./cat');
+
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
+  let cmdArr = [];
+  if (cmd.includes(' ')) {
+    cmdArr = cmd.split(' ');
+  }
 
   if (cmd === 'pwd') {
     required();
-
   }
-  else if (cmd === 'ls'){
-    lsRequired()
+  if (cmd === 'ls') {
+    lsRequired();
+  }
+  if (cmdArr[0] === 'cat') {
+    catRequired(cmdArr[1]);
   }
 
   process.stdout.write('You typed: ' + cmd);
   process.stdout.write('\nprompt > ');
 })
+
