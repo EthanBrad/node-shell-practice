f16ce1ba56d957174ac4b1da6d33dffea0ea3543
Mike <michael2kaminski@gmail.com>
2022-03-04 15:05:27 -0500

    ls

--- bash.js
+++ bash.js
@@ -1,10 +1,16 @@
+let required = require('./pwd');
+let lsRequired = require ('./ls')
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
 
   if (cmd === 'pwd') {
-    process.stdout.write(process.cwd());
+    required();
+
+  }
+  else if (cmd === 'ls'){
+    lsRequired()
   }
 
   process.stdout.write('You typed: ' + cmd);
